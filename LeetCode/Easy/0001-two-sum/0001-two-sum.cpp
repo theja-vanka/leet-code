f52042aef8957671108cb442a9cd066a85d19b0c
Krishnatheja Vanka <theja.vanka@gmail.com>
2025-09-19 15:12:34 +0530

    Time: 0 ms (100%), Space: 14.9 MB (20.05%) - LeetHub

---
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> um;
+        for(int i=0; i<nums.size(); i++){
+            int diff = target - nums[i];
+            if(um.find(diff) != um.end())
+                return {um[diff], i};
+            um[nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+};
