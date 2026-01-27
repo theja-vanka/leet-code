0042201e50fbf4a9402af43776bee07925171e3d
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-01-28 03:33:07 +0530

    Time: 11 ms (88.38%), Space: 19.3 MB (27.57%) - LeetHub

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,22 +4,16 @@ class Solution:
         left: int = 0
         right: int = 0
         max_len: int = 0
-        current_len: int = 0
+        char_map: dict = {}
 
-        save_set: set = set()
-
-        while(right < len(s)):
-            if s[right] not in save_set:
-                save_set.add(s[right])
-                current_len += 1
-                right += 1
+        while right < len(s):
+            if s[right] not in char_map:
+                char_map[s[right]] = right
             else:
-                save_set.remove(s[left])
-                left += 1
-                max_len = max(max_len, current_len)
-                current_len -= 1
-        
-        max_len = max(max_len, current_len)
+                left = max(left, char_map[s[right]] + 1)
+                char_map[s[right]] = right
+            max_len = max(max_len, right - left + 1)
+            right += 1
         
         return max_len
 
