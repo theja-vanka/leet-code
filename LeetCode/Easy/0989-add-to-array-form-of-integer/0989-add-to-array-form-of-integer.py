6d9e3edc4483acc42ac1f2696eab48d62ea4da7a
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-03-10 16:17:54 +0530

    Time: 7 ms (82.78%), Space: 19.5 MB (96.94%) - LeetHub

--- LeetCode/Easy/0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ LeetCode/Easy/0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,16 +1,16 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        i = len(num) - 1
-        carry = 0
-        res = []
 
-        while i >= 0 or k > 0 or carry > 0:
-            if i >= 0:
-                carry += num[i]
-                i -= 1
-            carry += k % 10
-            k //= 10
-            res.insert(0, carry % 10)
-            carry //= 10
+        carry: int = k
+        for index in range(len(num)-1, -1, -1):
+            carry, num[index] = divmod(num[index] + carry, 10)
+        
 
-        return res
+        while carry:
+            carry, base = divmod(carry, 10)
+            num.insert(0, base)
+        
+        return num
+
+        
+        
