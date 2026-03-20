9f3c9612121147c0c5dfd9fc5c5cb8b930d5e365
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-03-20 17:23:16 +0530

    Time: 23 ms (90.14%), Space: 23 MB (58.2%) - LeetHub

---
+++ LeetCode/Easy/0706-design-hashmap/0706-design-hashmap.py
@@ -0,0 +1,34 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+        
+
+    def put(self, key: int, value: int) -> None:
+        index = key % self.size
+        for i, (k, v) in enumerate(self.buckets[index]):
+            if k == key:
+                self.buckets[index][i] = (key, value)
+                return
+        self.buckets[index].append((key, value))
+
+    def get(self, key: int) -> int:
+        index = key % self.size
+        for k, v in self.buckets[index]:
+            if k == key:
+                return v
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = key % self.size
+        for i, (k, v) in enumerate(self.buckets[index]):
+            if k == key:
+                del self.buckets[index][i]
+                return
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
