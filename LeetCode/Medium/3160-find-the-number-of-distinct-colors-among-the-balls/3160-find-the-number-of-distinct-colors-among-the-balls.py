eda5c6b50a5a1b93d7fe6267a9e7477d14cfe262
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-03-23 02:38:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.py
+++ LeetCode/Medium/3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.py
@@ -5,19 +5,14 @@ class Solution:
         color_counter: dict = {}
         result: list = []
 
-        for q in queries:
-            ball, color = q[0], q[1]
-
-            if ball in ball_to_color:
-                old_color = ball_to_color[ball]
-                color_counter[old_color] -= 1
-                if color_counter[old_color] == 0:
-                    del color_counter[old_color]
+        for ball, color in queries:
+            if ball in ball_to_color and ball_to_color[ball] in color_counter:
+                color_counter[ball_to_color[ball]] -= 1
+                if color_counter[ball_to_color[ball]] == 0:
+                    del color_counter[ball_to_color[ball]]
             
-            if color not in color_counter:
-                color_counter[color] = 0
-            color_counter[color] += 1
             ball_to_color[ball] = color
+            color_counter[color] = color_counter.get(color, 0) + 1          
 
             result.append(len(color_counter.keys()))
         return result
