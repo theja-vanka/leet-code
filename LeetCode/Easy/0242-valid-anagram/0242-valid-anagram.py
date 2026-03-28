2e6ec671960a9ddf8771add5e1d637ee7815561a
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-03-29 03:35:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        hashmap: dict = {}
+        counter: int = [0] * 26
 
         if len(s) != len(t):
             return False
+        
 
         for _s in s:
-            hashmap[_s] = hashmap.get(_s, 0) + 1
+            counter[ord(_s) - ord('a')] += 1
         
         for _t in t:
-            if _t not in hashmap:
-                return False
-            else:
-                hashmap[_t] -= 1
-                if hashmap[_t] == 0:
-                    del hashmap[_t]
+            counter[ord(_t) - ord('a')] -= 1
         
-        return True
-        
+        return counter == [0] * 26
