2e02e876a7c3c58e2340199b6af282b9660ace58
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-04-01 04:40:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,10 +5,10 @@ class Solution:
         right: int = len(numbers) - 1
 
         while left < right:
-
-            if numbers[left] + numbers[right] == target:
+            index_sum = numbers[left] + numbers[right]
+            if index_sum == target:
                 return [left + 1, right + 1]
-            elif numbers[left] + numbers[right] < target:
+            elif index_sum < target:
                 left += 1
             else:
                 right -= 1
