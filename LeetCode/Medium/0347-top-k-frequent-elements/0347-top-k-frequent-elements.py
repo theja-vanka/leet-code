87d61ed4c9760a366c3956d243fdb2d0b4796243
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-04-23 05:53:44 +0530

    Time: 10 ms (25.78%), Space: 24.7 MB (25.23%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,23 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
+        frequency_list = [[] for i in range(len(nums) + 1)]
         hashmap: dict = {}
-        freq : List[list] = [[] for i in range(len(nums) + 1)]
 
         for num in nums:
             hashmap[num] = hashmap.get(num, 0) + 1
-
-        for key, value in hashmap.items():
-            freq[value].append(key)
         
-        result: list = []
-
-        for i in range(len(freq) - 1, 0, -1):
-            for v in freq[i]:
-                result.append(v)
-
+        for key, value in hashmap.items():
+            frequency_list[value].append(key)
+        
+        result = []
+        for index in range(len(frequency_list) - 1, -1, -1):
+            for n in frequency_list[index]:
+                result.append(n)
                 if len(result) == k:
                     return result
+            
+
+        
                     
 
 
