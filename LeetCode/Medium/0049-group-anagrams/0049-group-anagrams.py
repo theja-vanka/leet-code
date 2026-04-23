79e0360b24c881909ea592b171c46b1580c8a4df
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-04-23 05:41:11 +0530

    Time: 7 ms (98.53%), Space: 22.4 MB (45.58%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -2,19 +2,16 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
         hashmap: dict = {}
-
-        for s in strs:
-            _ = tuple(sorted(s))
-
-            if _ not in hashmap:
-                hashmap[_] = [s]
-            else:
-                hashmap[_].append(s)
-        
         result = []
+        for s in strs:
+            s_t = tuple(sorted(s))
 
+            if s_t not in hashmap:
+                hashmap[s_t] = [s]
+            else:
+                hashmap[s_t].append(s)
+        
         for value in hashmap.values():
             result.append(value)
 
         return result
-        
