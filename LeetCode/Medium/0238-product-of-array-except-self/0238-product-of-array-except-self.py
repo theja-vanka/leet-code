7e0addc8a06a04e0b26f8091a2715d3d59f71210
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-04-24 15:17:14 +0530

    Time: 18 ms (80.49%), Space: 25.4 MB (81.78%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,7 +1,7 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        result: list = [0] * len(nums)
+        result = [0] * len(nums)
         prefix = 1
 
         for i in range(len(nums)):
@@ -9,11 +9,15 @@ class Solution:
             prefix *= nums[i]
         
         postfix = 1
-        for i in range(len(nums)-1, -1, -1):
+
+        for i in range(len(nums) - 1, -1, -1):
             result[i] *= postfix
             postfix *= nums[i]
 
         return result
+
+
+        
 
 
         
