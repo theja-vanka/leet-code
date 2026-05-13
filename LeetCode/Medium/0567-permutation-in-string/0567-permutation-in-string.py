9a33c897501ac7067296bc195dd37c01cb617c1e
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-05-13 13:27:36 +0530

    Time: 14 ms (84.53%), Space: 19.4 MB (54.12%) - LeetHub

--- LeetCode/Medium/0567-permutation-in-string/0567-permutation-in-string.py
+++ LeetCode/Medium/0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,18 +1,37 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
 
-        if len(s1) > len(s2):
-            return False
+        s1_count, s2_count = [0] * 26, [0] * 26
 
-        counter_s1 = Counter(s1)
-        window = len(s1)
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
 
-        for i in range(0, len(s2) - window + 1, 1):
-            _ = Counter(s2[i:i+window])
-            if _ == counter_s1:
+        matches = 0 
+        for i in range(26):
+            matches += (1 if s1_count[i] == s2_count[i] else 0)
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
                 return True
-        
-        return False
-        
-        
+
+            index = ord(s2[r]) - ord('a')
+            s2_count[index] += 1
+            if s2_count[index] == s1_count[index]:
+                matches += 1
+            elif s1_count[index] + 1 == s2_count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            s2_count[index] -= 1
+            if s2_count[index] == s1_count[index]:
+                matches += 1
+            elif s1_count[index] - 1 == s2_count[index]:
+                matches -= 1
+
+            l += 1
+        return matches == 26
+
         
