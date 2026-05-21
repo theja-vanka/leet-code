16bf5b3a86410c7db8c1c6e60d71632cc6840612
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-05-21 17:48:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (52.36%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
 
-        memo: dict = {}
-        memo[0] = 1
-        memo[1] = 1
-        memo[2] = 2
-
-
-        def dynamic(n):
-            if n not in memo:
-                memo[n] = dynamic(n-1) + dynamic(n-2)
-            return memo[n]
-
-        return dynamic(n) 
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
         
