b60b11600f82ae117912bf97dd69234b719ade5a
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-07-10 10:50:54 +0530

    Time: 3 ms (65.63%), Space: 19.4 MB (40.72%) - LeetHub

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,33 +6,33 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        carry, base = divmod(l1.val + l2.val, 10)
-        retl: Optional[ListNode] = ListNode(base)
-        l1 = l1.next
-        l2 = l2.next
-
-        return_ln = retl
-
+        dummy: Optional[ListNode] = ListNode()
+        result: Optional[ListNode] = dummy
+        carry: int = 0
         while l1 and l2:
-            carry, base = divmod(l1.val + l2.val + carry, 10)
-            retl.next = ListNode(base)
-            retl = retl.next
+            _ = l1.val + l2.val + carry
+            dummy.next = ListNode(_%10)
+            carry = _ // 10
+            dummy = dummy.next
             l1 = l1.next
             l2 = l2.next
         
         while l1:
-            carry, base = divmod(l1.val + carry, 10)
-            retl.next = ListNode(base)
-            retl = retl.next
+            _ = l1.val + carry
+            dummy.next = ListNode(_%10)
+            carry = _ // 10
+            dummy = dummy.next
             l1 = l1.next
         
         while l2:
-            carry, base = divmod(l2.val + carry, 10)
-            retl.next = ListNode(base)
-            retl = retl.next
+            _ = l2.val + carry
+            dummy.next = ListNode(_%10)
+            carry = _ // 10
+            dummy = dummy.next
             l2 = l2.next
         
         if carry:
-            retl.next = ListNode(carry)
+            dummy.next = ListNode(carry)
+
+        return result.next
         
-        return return_ln
