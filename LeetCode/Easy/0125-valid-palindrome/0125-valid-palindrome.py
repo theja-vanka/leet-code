3825552d0bd017fa83b271093cc1d8911576388b
Krishnatheja Vanka <theja.vanka@gmail.com>
2026-08-31 15:23:40 +0530

    Time: 7 ms (81.13%), Space: 19.6 MB (77.51%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,21 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        cleaned_string: str = ""
-
-        for c in s:
-            if c.isalnum():
-                cleaned_string += c.lower()
-
-
-        left: int = 0
-        right: int = len(cleaned_string) - 1
-
+        left:int = 0
+        right:int = len(s) - 1
+        
         while left < right:
-            if cleaned_string[left] != cleaned_string[right]:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
             left += 1
             right -= 1
         
         return True
-    
